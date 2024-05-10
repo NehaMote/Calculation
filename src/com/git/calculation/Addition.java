00a0de6987dd90b809cf113432762eb314fde58a
NehaMote <nehamote18@gmail.com>
2024-05-10 09:41:59 +0530

    Added changes

--- src/com/git/calculation/Addition.java
+++ src/com/git/calculation/Addition.java
@@ -2,7 +2,10 @@ package com.git.calculation;
 
 public class Addition {
   public static void add() {
-	  
+	  int a=10;
+	  int b=20;
+	  int c=a+b;
+	  System.out.println(c);
   }
 	public static void main(String[] args) {
 		add();
