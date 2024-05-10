27db02de27e82334e7089592fc9cd7d7c1a5af1d
NehaMote <nehamote18@gmail.com>
2024-05-10 09:45:40 +0530

    Add changes in Subtraction.java file by dev1

--- src/com/git/calculation/Subtraction.java
+++ src/com/git/calculation/Subtraction.java
@@ -2,6 +2,10 @@ package com.git.calculation;
 
 public class Subtraction {
   public static void sub() {
+	  int a=10;
+	  int b=20;
+	  int c=a-b;
+	  System.out.println(c);
 	  
   }
 	public static void main(String[] args) {
