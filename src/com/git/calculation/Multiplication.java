69c087ba9f2b63f798e35b2800125e587c032096
NehaMote <nehamote18@gmail.com>
2024-05-10 09:56:33 +0530

    added multiplication.java and test.java file by dev2

---
+++ src/com/git/calculation/Multiplication.java
@@ -0,0 +1,14 @@
+package com.git.calculation;
+
+public class Multiplication { 
+	public static void mul() {
+		int a=10;
+		int b=20;
+		int c=a*b;
+		System.out.println(c);
+	}
+public static void main(String[] args) {
+	mul();
+	
+}
+}
